515ef42118ac736eaa1304a8c1e3b3440d7be327
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2022-04-09 10:07:29 -0600

    first commit, several intro assignments

---
+++ ch0/hello.py
@@ -0,0 +1,5 @@
+# Hello world python script for "Peer Review: Installing and Running Python Screen Shots"
+# Author: Gerardo Hidalgo-Cuellar
+# Date: 4/8/2022
+
+print("Hello, world!")
---
+++ ch1/hello.py
@@ -0,0 +1,6 @@
+# Hello world python script for Ch1 "Autograder: Write Hello World"
+# nearly identical program to ch1
+# Author: Gerardo Hidalgo-Cuellar
+# Date: 4/8/2022
+
+print("hello world")
